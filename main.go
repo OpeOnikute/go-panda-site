cb0163d2d69eb895f82bf22a9c7af8ed25f9a128
Opeyemi <opeyemionikute@yahoo.com>
2020-06-21 18:20:39 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/opeonikute/panda"
+)
+
+type HomePageData struct {
+	PandaFound bool
+	ImageURL   string
+	Source     string
+	FileName   string
+}
+
+func main() {
+	port := 5399
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		data := HomePageData{
+			PandaFound: false,
+		}
+
+		panda, err := getPandaOfTheDay()
+
+		if err != nil {
+			fmt.Print(err)
+		} else {
+			data.PandaFound = true
+			data.ImageURL = panda.URL
+			data.Source = panda.Source
+			data.FileName = panda.FileName
+		}
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+
+		tmpl.Execute(w, data)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Printf("Server listening on port: %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func getPandaOfTheDay() (panda.Entry, error) {
+	var en panda.Entry
+	goPanda := panda.GoPanda{
+		Config: panda.Settings{
+			MongoURL: os.Getenv("MONGO_URL"),
+			MongoDB:  os.Getenv("MONGO_DATABASE"),
+		},
+	}
+
+	tm := time.Now()
+	res, err := goPanda.GetPOD(tm)
+
+	if err != nil {
+		return en, err
+	}
+
+	return res, nil
+}
